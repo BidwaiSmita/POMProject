1e17066e2b1df53b6740e689fb3e53eebf03c2e1
SmitaBidwai <smitabidwai2509@gmail.com>
2025-05-20 21:39:12 +0530

    POM project

---
+++ src/test/java/org/example/POM_OrangeHRM_001.java
@@ -0,0 +1,18 @@
+package org.example;
+
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+
+public class POM_OrangeHRM_001 {
+
+
+    @Test
+    public void baseMethod(){
+
+        WebDriver driver = new ChromeDriver();
+
+    }
+}
